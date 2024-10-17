48a65fd0fe92ca0eb8e222a03387021c66ad47b3
MilosPeulic <milos.peulic@automaticity.rs>
2024-10-17 15:48:01 +0200

    minor update

--- automaticity_academy/src/test/java/automaticity_academy/tests/api/Registration.java
+++ automaticity_academy/src/test/java/automaticity_academy/tests/api/Registration.java
@@ -16,14 +16,11 @@ public class Registration {
     public void successfullRegistration() {
         RegistrationApi registration = new RegistrationApi();
         String[][] userJson = User.generateRandomUserForRegistration();
-        // System.out.println(userJson);
         Map<String, Object> userBody = JsonGenerator.generateJson(userJson);
-        System.out.println(userBody);
         Response response = registration.sendApiRequest(ApiConstans.HttpMethods.POST.getMethod(),
                 ApiConstans.urlEndpoint.REGISTER,
                 null,
                 userBody);
-        // System.out.println(response.toString());
         registration.checkIfUserRegistrated(response, userBody);
         registration.checkStatusCode(response, 200);
         registration.checkStatusMessage(response, ApiConstans.StatusMessages.USER_CREATED);
