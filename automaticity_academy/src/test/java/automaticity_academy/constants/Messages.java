6bdd16cad6491e9394014193833874e84e092da9
MilosPeulic <milos.peulic@automaticity.rs>
2024-11-22 08:33:06 +0100

    Dasboard Cucumber tests created

--- automaticity_academy/src/test/java/automaticity_academy/constants/Messages.java
+++ automaticity_academy/src/test/java/automaticity_academy/constants/Messages.java
@@ -4,4 +4,5 @@ public class Messages {
 
     public static final String BUY_SOME_STUFF_BRUH = "Buy some stuff bruh";
     public static final String SUCCESSFULLY_REGISTERED = "Successfully registered!";
+    public static final String PRODUCT_ADDED = "Product added successfully!";
 }
